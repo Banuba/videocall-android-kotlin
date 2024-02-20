98350474da5dd44df99ea520c7a3809475990272
Piotr Kulbaka <petr.kulbaka@banuba.com>
2024-02-20 16:35:24 +0300

    update

--- app/src/main/java/com/banuba/sdk/example/videocall/MainActivity.kt
+++ app/src/main/java/com/banuba/sdk/example/videocall/MainActivity.kt
@@ -2,7 +2,6 @@ package com.banuba.sdk.example.videocall
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
 import android.view.SurfaceView
 import android.widget.Toast
@@ -10,9 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.banuba.sdk.frame.FramePixelBuffer
 import com.banuba.sdk.input.CameraDevice
-import com.banuba.sdk.input.CameraDeviceConfigurator
 import com.banuba.sdk.input.CameraInput
-import com.banuba.sdk.manager.BanubaSdkManager
 import com.banuba.sdk.output.FrameOutput
 import com.banuba.sdk.output.IOutput
 import com.banuba.sdk.output.SurfaceOutput
@@ -26,31 +23,32 @@ import io.agora.rtc2.video.VideoCanvas
 import io.agora.rtc2.video.VideoEncoderConfiguration
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     companion object {
-        private const val MASK_NAME = "TrollGrandma"
-
+        private const val MASK_NAME = "effects/TrollGrandma"
         private const val REQUEST_CODE_PERMISSIONS = 1001
-
         private val REQUIRED_PERMISSIONS = arrayOf(
             Manifest.permission.CAMERA,
             Manifest.permission.RECORD_AUDIO
         )
     }
 
+    // The player executes the main pipeline
     private val player by lazy(LazyThreadSafetyMode.NONE) {
         Player()
     }
 
+    // This camera device will pass frames to the CameraInput
     private val cameraDevice by lazy(LazyThreadSafetyMode.NONE) {
         CameraDevice(requireNotNull(this.applicationContext), this@MainActivity)
     }
 
+    // The result will be displayed on the surface
     private val surfaceOutput by lazy(LazyThreadSafetyMode.NONE) {
         SurfaceOutput(localSurfaceView.holder)
     }
 
+    // The result also will be passed to agora as an array of pixels
     private val frameOutput by lazy(LazyThreadSafetyMode.NONE) {
         FrameOutput(object : FrameOutput.IFramePixelBufferProvider {
             override fun onFrame(output: IOutput, pb: FramePixelBuffer?) {
@@ -59,21 +57,11 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private val maskUri by lazy(LazyThreadSafetyMode.NONE) {
-        Uri.parse(BanubaSdkManager.getResourcesBase())
-            .buildUpon()
-            .appendPath("effects")
-            .appendPath(MASK_NAME)
-            .build()
+    // Agora RTC engine makes a videocall
+    private val agoraRtc: RtcEngine by lazy(LazyThreadSafetyMode.NONE) {
+        RtcEngine.create(this, AGORA_APP_ID, agoraEventHandler)
     }
 
-    private val agoraRtc: RtcEngine by lazy(LazyThreadSafetyMode.NONE) {
-        RtcEngine.create(
-            this,
-            AGORA_APP_ID,
-            agoraEventHandler
-        )
-    }
     private val agoraEventHandler = object : IRtcEngineEventHandler() {
         override fun onFirstRemoteVideoDecoded(uid: Int, width: Int, height: Int, elapsed: Int) {
             runOnUiThread {
@@ -86,14 +74,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        configureSdkManager()
-        setupLocalVideo()
+        configureBanubaSdk()
     }
 
     private fun start() {
         configureRtcEngine()
-        joinRtcChannel()
         cameraDevice.start()
     }
 
@@ -131,11 +116,7 @@ class MainActivity : AppCompatActivity() {
         frameOutput.close()
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        results: IntArray
-    ) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, results: IntArray) {
         if (checkAllPermissionsGranted()) {
             start()
         } else {
@@ -144,33 +125,35 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun configureSdkManager() {
-        val playerTouchListener = PlayerTouchListener(this, player)
-        localSurfaceView.setOnTouchListener(playerTouchListener)
+    private fun configureBanubaSdk() {
+        localSurfaceView.setOnTouchListener(PlayerTouchListener(this, player))
+        // Turn off audio effects
         player.setEffectVolume(0F)
-        player.use(CameraInput(cameraDevice))
-        player.addOutput(surfaceOutput)
-        player.addOutput(frameOutput)
-        player.loadAsync(maskUri.toString())
+        // Set layer will take input frames from and where the player will display the result
+        player.use(CameraInput(cameraDevice), arrayOf(surfaceOutput, frameOutput))
+        // Loading the effect
+        player.loadAsync(MASK_NAME)
+
+        localSurfaceView.setZOrderMediaOverlay(true)
     }
 
     private fun configureRtcEngine() {
+        // Initialize the Agora RTC SDK
         agoraRtc.setExternalVideoSource(true, false, Constants.ExternalVideoSourceType.VIDEO_FRAME)
-        val videoEncoderConfiguration = VideoEncoderConfiguration(
+        agoraRtc.setChannelProfile(Constants.CHANNEL_PROFILE_LIVE_BROADCASTING)
+        agoraRtc.setClientRole(Constants.CLIENT_ROLE_BROADCASTER)
+        agoraRtc.setVideoEncoderConfiguration(VideoEncoderConfiguration(
             VideoEncoderConfiguration.VD_1280x720,
             VideoEncoderConfiguration.FRAME_RATE.FRAME_RATE_FPS_30,
             VideoEncoderConfiguration.STANDARD_BITRATE,
             VideoEncoderConfiguration.ORIENTATION_MODE.ORIENTATION_MODE_FIXED_PORTRAIT
-        )
-        agoraRtc.setChannelProfile(Constants.CHANNEL_PROFILE_LIVE_BROADCASTING)
-        agoraRtc.setClientRole(Constants.CLIENT_ROLE_BROADCASTER)
-        agoraRtc.setVideoEncoderConfiguration(videoEncoderConfiguration)
+        ))
         agoraRtc.enableVideo()
         agoraRtc.enableAudio()
-    }
 
-    private fun setupLocalVideo() {
-        localSurfaceView.setZOrderMediaOverlay(true)
+        // Join a video call
+        agoraRtc.setDefaultAudioRoutetoSpeakerphone(true)
+        agoraRtc.joinChannel(AGORA_CLIENT_TOKEN, AGORA_CHANNEL_ID, null, 0)
     }
 
     private fun setupRemoteVideo(uid: Int): SurfaceView {
@@ -178,11 +161,6 @@ class MainActivity : AppCompatActivity() {
         val videoCanvas = VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_HIDDEN, uid)
         agoraRtc.setupRemoteVideo(videoCanvas)
         return surfaceView
-    }
-
-    private fun joinRtcChannel() {
-        agoraRtc.setDefaultAudioRoutetoSpeakerphone(true)
-        agoraRtc.joinChannel(AGORA_CLIENT_TOKEN, AGORA_CHANNEL_ID, null, 0)
     }
 
     private fun pushCustomFrame(pb: FramePixelBuffer) {
